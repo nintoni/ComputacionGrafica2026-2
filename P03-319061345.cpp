250ae09ca618ad0a837d73bb24bda92c85c9c7ce
Nintoni <106836541+nintoni@users.noreply.github.com>
2026-03-05 05:26:28 -0600

    Add files via upload

---
+++ P03-319061345.cpp
@@ -0,0 +1,310 @@
+//práctica 3: Modelado Geométrico y Cámara Sintética.
+#include <stdio.h>
+#include <string.h>
+#include<cmath>
+#include<vector>
+#include <glew.h>
+#include <glfw3.h>
+//glm
+#include<glm.hpp>
+#include<gtc\matrix_transform.hpp>
+#include<gtc\type_ptr.hpp>
+#include <gtc\random.hpp>
+//clases para dar orden y limpieza al còdigo
+#include"Mesh.h"
+#include"Shader.h"
+#include"Sphere.h"
+#include"Window.h"
+#include"Camera.h"
+//tecla E: Rotar sobre el eje X
+//tecla R: Rotar sobre el eje Y
+//tecla T: Rotar sobre el eje Z
+
+
+using std::vector;
+
+//Dimensiones de la ventana
+const float toRadians = 3.14159265f / 180.0; //grados a radianes
+const float PI = 3.14159265f;
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+Camera camera;
+Window mainWindow;
+vector<Mesh*> meshList;
+vector<Shader>shaderList;
+//Vertex Shader
+static const char* vShader = "shaders/shader.vert";
+static const char* fShader = "shaders/shader.frag";
+static const char* vShaderColor = "shaders/shadercolor.vert";
+Sphere sp = Sphere(1.0, 20, 20); //recibe radio, slices, stacks
+
+
+
+
+void CrearCubo()
+{
+	unsigned int cubo_indices[] = {
+		// front
+		0, 1, 2,
+		2, 3, 0,
+		// right
+		1, 5, 6,
+		6, 2, 1,
+		// back
+		7, 6, 5,
+		5, 4, 7,
+		// left
+		4, 0, 3,
+		3, 7, 4,
+		// bottom
+		4, 5, 1,
+		1, 0, 4,
+		// top
+		3, 2, 6,
+		6, 7, 3
+	};
+
+	GLfloat cubo_vertices[] = {
+		// front
+		-0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		// back
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f
+	};
+	Mesh* cubo = new Mesh();
+	cubo->CreateMesh(cubo_vertices, cubo_indices, 24, 36);
+	meshList.push_back(cubo);
+}
+
+// Pirámide triangular regular
+void CrearPiramideTriangular()
+{
+	unsigned int indices[] = {
+		0,1,2,
+		0,3,1,
+		0,2,3,
+		1,3,2
+	};
+
+	GLfloat vertices[] = {
+		0.5f,  0.5f,  0.5f,   // 0
+	   -0.5f, -0.5f,  0.5f,   // 1
+	   -0.5f,  0.5f, -0.5f,   // 2
+		0.5f, -0.5f, -0.5f    // 3
+	};
+
+	Mesh* obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj1);
+}
+
+void CreateShaders()
+{
+	Shader* shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+
+	Shader* shader2 = new Shader();
+	shader2->CreateFromFiles(vShaderColor, fShader);
+	shaderList.push_back(*shader2);
+}
+
+
+
+int main()
+{
+	mainWindow = Window(800, 800);
+	mainWindow.Initialise();
+	//Cilindro y cono reciben resolución (slices, rebanadas) y Radio de circunferencia de la base y tapa
+
+	CrearCubo();//índice 0 en MeshList
+	CrearPiramideTriangular();//índice 1 en MeshList
+	CreateShaders();
+
+
+
+	/*Cámara se usa el comando: glm::lookAt(vector de posición, vector de orientación, vector up));
+	En la clase Camera se reciben 5 datos:
+	glm::vec3 vector de posición,
+	glm::vec3 vector up,
+	GlFloat yaw rotación para girar hacia la derecha e izquierda
+	GlFloat pitch rotación para inclinar hacia arriba y abajo
+	GlFloat velocidad de desplazamiento,
+	GlFloat velocidad de vuelta o de giro
+	Se usa el Mouse y las teclas WASD y su posición inicial está en 0,0,1 y ve hacia 0,0,-1.
+	*/
+
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.3f);
+
+
+	GLuint uniformProjection = 0;
+	GLuint uniformModel = 0;
+	GLuint uniformView = 0;
+	GLuint uniformColor = 0;
+	glm::mat4 projection = glm::perspective(glm::radians(60.0f), mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
+	//glm::mat4 projection = glm::ortho(-1, 1, -1, 1, 1, 10);
+
+	//Loop mientras no se cierra la ventana
+	sp.init(); //inicializar esfera
+	sp.load();//enviar la esfera al shader
+
+	glm::mat4 model(1.0);//Inicializar matriz de Modelo 4x4
+
+	glm::vec3 color = glm::vec3(0.0f, 0.0f, 0.0f); //inicializar Color para enviar a variable Uniform;
+
+	while (!mainWindow.getShouldClose())
+	{
+
+		GLfloat now = glfwGetTime(); //tiempo estandarizado para rotaciones a la misma velocidad 
+		deltaTime = now - lastTime;
+		deltaTime += (now - lastTime) / limitFPS;
+		lastTime = now;
+		//Recibir eventos del usuario
+		glfwPollEvents();
+		//Cámara , esta libre a donde apuntas y te mueves hacia alla vas
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);//funcion de teclado y mouse
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+		//Limpiar la ventana
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Se agrega limpiar el buffer de profundidad
+		shaderList[0].useShader(); //solo usar un shader
+		uniformModel = shaderList[0].getModelLocation();
+		uniformProjection = shaderList[0].getProjectLocation();
+		uniformView = shaderList[0].getViewLocation();
+		uniformColor = shaderList[0].getColorLocation();
+
+		//cubo reusado jajs
+		model = glm::mat4(1.0);
+		//Traslación inicial para posicionar en -Z a los objetos
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		//otras transformaciones para el objeto
+		//model = glm::scale(model, glm::vec3(0.5f,0.5f,0.5f));
+		model = glm::rotate(model, glm::radians(mainWindow.getrotax()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getrotay()), glm::vec3(0.0f, 1.0f, 0.0f));  //al presionar la tecla Y se rota sobre el eje y
+		model = glm::rotate(model, glm::radians(mainWindow.getrotaz()), glm::vec3(0.0f, 0.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		//la línea de proyección solo se manda una vez a menos que en tiempo de ejecución
+		//se programe cambio entre proyección ortogonal y perspectiva
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+		color = glm::vec3(1.0f, 0.0f, 0.0f); //color magenta del cubo
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+		//meshList[0]->RenderMesh(); //dibuja cubo y pirámide triangular
+
+
+		//piramide base
+		model = glm::mat4(1.0f);
+		color = glm::vec3(0.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+
+		model = glm::scale(model, glm::vec3(9.0f, 9.0f, 9.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+
+		////EJES x y z
+		model = glm::mat4(1.0f);
+		color = glm::vec3(1.0f, 0.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(6.0f, 0.0f, 0.0f));
+
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+
+		model = glm::mat4(1.0f);
+		color = glm::vec3(1.0f, 0.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(5.0f, 0.0f, 0.0f));
+
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+
+		model = glm::mat4(1.0f);
+		color = glm::vec3(0.0f, 1.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(0.0f, 6.0f, 0.0f));
+
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+
+		model = glm::mat4(1.0f);
+		color = glm::vec3(0.0f, 1.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(0.0f, 5.0f, 0.0f));
+
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+
+		model = glm::mat4(1.0f);
+		color = glm::vec3(0.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 6.0f));
+
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+	
+		model = glm::mat4(1.0f);
+		color = glm::vec3(0.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 5.0f));
+
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+
+		//cara 0
+		model = glm::mat4(1.0f);
+		color = glm::vec3(1.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(4.5f, 0.5f, 1.5f));
+
+		model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+		//
+		model = glm::mat4(1.0f);
+		color = glm::vec3(1.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(3.0f, 0.5f, 3.0f));
+
+		model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+		//
+		model = glm::mat4(1.0f);
+		color = glm::vec3(1.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color)); //para cambiar el color del objetos q se actualice e shader
+
+		model = glm::translate(model, glm::vec3(1.5f, 0.5f, 4.5f));
+
+		model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));//FALSE ES PARA QUE NO SEA TRANSPUESTA
+		meshList[1]->RenderMeshGeometry();
+
+		//-----------------------------------------------------------------------------------------------------------------------
+		glUseProgram(0);
+		mainWindow.swapBuffers();
+	}
+	return 0;
+}
